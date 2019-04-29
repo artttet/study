cf8ab316f8096a775828183b28d6cd7613cc21ac
artas <chiglincev.artem@gmail.com>
2019-04-29 08:30:15 +0600

    2 and 3 assembler

---
+++ assembler-2semestr/Rakicky_2-6.cpp
@@ -0,0 +1,70 @@
+﻿
+#include "pch.h"
+#include "stdio.h"
+#include <locale.h>
+
+int summ(int* M, bool modul) {
+	int result = 0;
+	for (int i = 0; i < 3; i++) {
+		if (modul) {
+			if (M[i] < 0) M[i] = 0 - M[i];
+		}
+		result += M[i];
+	}
+	return result;
+}
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+	int A[3], B[3], x, y;
+	int result = 0;
+	printf("Сейчас посчитается: Summ(|Ai|)*X + Summ(Bi)*Y \n");
+	for (int i = 0; i < 3;i++) {
+		printf("Введите значение элемента A[%i] \n", i);
+		scanf_s("%i",&A[i]);
+	}
+	for (int i = 0; i < 3; i++) {
+		printf("Введите значение элемента B[%i] \n", i);
+		scanf_s("%i", &B[i]);
+	}
+	printf("Введите значение X \n");
+	scanf_s("%i", &x);
+	printf("Введите значение Y \n");
+	scanf_s("%i", &y);
+	result = summ(A, true)*x + summ(B, false)*y;
+
+	_asm {
+		mov eax, 0
+		mov esi, 0
+		mov ecx, 3
+		lea ebx, A
+
+			loopA:
+		mov esi,[ebx]
+		cmp esi, 0
+		jge m1
+		neg esi
+		
+			m1:
+		add eax, esi
+		add ebx,4
+		loop loopA
+		imul x
+		mov x,eax
+
+		mov eax,0
+		lea ebx, B
+		mov ecx,3 
+
+			loopB:
+		add eax, [ebx]
+		add ebx,4
+		loop loopB
+		imul y
+		add x,eax
+	}
+	printf("\n Результат программы на языке Ассемблер: %i \n", x);
+	printf("Результат программы на языке С: %i \n", result);
+
+
+}
---
+++ assembler-2semestr/Rakicky_3-17.cpp
@@ -0,0 +1,76 @@
+﻿
+#include "pch.h"
+#include <stdio.h>
+#include <locale.h>
+
+void print_bin(short chislo, char bykva) {
+	printf("%c --- ", bykva);
+	for (int i = 15; i >= 0;i--) {
+		if (i == 11 || i == 7 || i == 3)printf(" ");
+		if (chislo &(1 << i)) {
+			printf("1");
+		}
+		else printf("0");
+		
+	}
+}
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+	short a, b;
+	short c = 0;
+
+	printf("Введите число a: \n");
+	scanf_s("%hi", &a);
+	while (a < 0 || a > 255) {
+		printf("Введите число a: \n");
+		scanf_s("%hi", &a);
+	}
+
+	printf("Введите число b: \n");
+	scanf_s("%hi", &b);
+	while (b == 0 || b < 0|| b > 255) {
+		printf("Введите число b: \n");
+		scanf_s("%hi", &b);
+	}
+	int test= 0;
+	int d = 0;
+	_asm {
+		mov ax, a
+		cwd
+		idiv b
+		mov a, ax
+		mov b, dx
+		
+		bsr esi, dx
+		jz metka2
+		mov d,esi
+
+		bsr cl,dx
+		inc cl
+		shl ax, cl
+		
+		metka:
+		mov esi,0
+		bsr esi, dx
+		bts ax, esi
+		btr dx, esi
+
+		bsr esi, dx
+		jnz metka
+			metka2:
+			mov c, ax
+		
+	}
+	
+	printf("\n Result:\n\n A(Частное) - %hi;\n B(Остаток) - %hi;\n C - %i \n\n", a,b,c);
+
+	print_bin(a,'A');
+	printf("\n");
+	print_bin(b,'B');
+	printf("\n");
+	print_bin(c, 'C');
+	printf("\n");
+
+}
+
